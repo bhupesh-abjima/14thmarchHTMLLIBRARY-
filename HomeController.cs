acce3393ae3b05830498b25c5eb1e99f64831926
bhupesh-abjima <97227019+bhupesh-abjima@users.noreply.github.com>
2022-03-14 22:08:34 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTCData;
+
+namespace PTC.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      TrainingProductViewModel vm = new TrainingProductViewModel();
+
+      vm.HandleRequest();
+
+      return View(vm);
+    }
+
+    [HttpPost]
+    public ActionResult Index(TrainingProductViewModel vm)
+    {
+      vm.IsValid = ModelState.IsValid;
+      vm.HandleRequest();
+
+      if (vm.IsValid) {
+      
+        ModelState.Clear();
+      }
+      else {
+        foreach (KeyValuePair<string, string> item in vm.ValidationErrors) {
+          ModelState.AddModelError(item.Key, item.Value);
+        }
+      }
+
+      return View(vm);
+    }
+
+    public ActionResult About()
+    {
+      return View();
+    }
+  }
+}
---
+++ HtmlExtensionsCommon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace PTC
+{
+  public static class HtmlExtensionsCommon
+  {
+    public enum HtmlButtonTypes
+    {
+      submit,
+      button,
+      reset
+    }
+
+    public static void AddName(TagBuilder tb, string name, string id) {
+      if (!string.IsNullOrWhiteSpace(name)) {
+        // Ensure we have valid name
+        name = TagBuilder.CreateSanitizedId(name);
+
+        if (string.IsNullOrWhiteSpace(id)) {
+          // Generate valid 'id' attribute 
+          // from the 'name' attribute
+          tb.GenerateId(name);
+        }
+        else {
+          // Add 'id' to HTML
+          tb.MergeAttribute("id",
+              TagBuilder.CreateSanitizedId(id));
+        }
+
+        // Add 'name' to HTML
+        tb.MergeAttribute("name", name);
+      }
+    }
+  }
+}
